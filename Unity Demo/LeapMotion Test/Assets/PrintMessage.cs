1090173582e3752424cf31b1b8031b5b86b95207
Alexander Chen <alexanderchen711@gmail.com>
2021-04-08 17:22:33 -0700

    Initial Unity Demo
    Finger direction and basic gesture recognition

---
+++ Unity Demo/LeapMotion Test/Assets/PrintMessage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMessage : MonoBehaviour {
+    public void PrintActivateMessage() {
+        print ("Thumbs Up!");
+    }
+    public void PrintDeactivateMessage() {
+        print ("Deactivated");
+    }
+}
---
+++ Unity Demo/LeapMotion Test/Assets/PrintMessage2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMessage2 : MonoBehaviour {
+    public void PrintActivateMessage() {
+        print ("Pointing at Cube");
+    }
+    public void PrintDeactivateMessage() {
+        print ("Not Pointing at Cube");
+    }
+}
