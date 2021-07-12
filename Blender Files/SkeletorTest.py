718357cb273926b6697b594b1bb3b2f25c494b5e
Alexander Chen <alexanderchen711@gmail.com>
2021-07-11 19:51:16 -0700

    Add All Hand Gestures from Study

--- Blender Files/SkeletorTest.py
+++
@@ -1,8 +0,0 @@
-import skeletor as sk
-
-
-mesh = sk.example_mesh()
-fixed = sk.pre.fix_mesh(mesh, remove_disconnected=5, inplace=False)
-skel = sk.skeletonize.by_wavefront(fixed, waves=1, step_size=1)
-skel
-skel.show(mesh=False)
